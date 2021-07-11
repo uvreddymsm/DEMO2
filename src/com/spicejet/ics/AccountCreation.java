0e7283b4f055df2ee6365f59a3fdc0faf8f834cf
uvreddymsm <uvreddymsm@gmail.com>
2021-07-11 19:05:54 +0530

    Added Two Methods In Two Classes

--- src/com/spicejet/ics/AccountCreation.java
+++ src/com/spicejet/ics/AccountCreation.java
@@ -1,4 +1,9 @@
 package com.spicejet.ics;
 
 public class AccountCreation {
+
+
+    public void accountCreation() {
+
+    }
 }
--- src/com/spicejet/ics/AccountUpdation.java
+++ src/com/spicejet/ics/AccountUpdation.java
@@ -1,4 +1,9 @@
 package com.spicejet.ics;
 
 public class AccountUpdation {
+
+    public void accountUpdation() {
+
+
+    }
 }
